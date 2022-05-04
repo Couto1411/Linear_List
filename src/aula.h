c30ee40a3917853de1cc503dfc9dc41c1aba5875
Couto1411 <gabriel.couto14@hotmail.com>
2022-05-04 15:19:07 -0300

    Funcionando excluindo ponteiros

---
+++ src/aula.h
@@ -0,0 +1,37 @@
+#ifndef AULA
+#define AULA
+
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXTAM 100
+
+typedef struct Bloco Bloco;
+typedef struct Item{
+    char value[100];
+}Item;
+
+struct Bloco{
+    Item dado;
+    Bloco *prox;
+};
+
+typedef struct Lista{
+    Bloco* cabeca;
+    Bloco* cauda;
+}Lista;
+
+int lenLista;
+
+void FLVazia(Lista *l);
+void preencheLista(Lista *l,char f[80], bool novo);
+void insertLista(Lista *l);
+void removeListaItem(Lista *l);
+void removeRepet(Lista *l);
+void printLista(Lista *l);
+void preencheArquivo(FILE* arq);
+void atualizaArquivo(Lista *l,char nomearquvio[80]);
+#endif
